11e4ccc5df82e00082f56503d54c80615b2dbaee
Mudabbirul Saad <71183979+MudabbirulSaad@users.noreply.github.com>
2026-08-02 11:56:57 +1000

    Make deployment base configurable

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  base: '/cos30043/student/w9/',
-  plugins: [vue()],
-  test: {
-    environment: 'jsdom',
-    globals: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    plugins: [vue()],
+    test: {
+      environment: 'jsdom',
+      globals: true
+    }
   }
 })
