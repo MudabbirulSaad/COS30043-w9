4a9dd44a159aa1e95c1e79e161c632720e0263ef
Mudabbirul Saad <71183979+MudabbirulSaad@users.noreply.github.com>
2026-08-02 11:41:19 +1000

    Remove public student identifier

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/cos30043/s105281389/w9/',
+  base: '/cos30043/student/w9/',
   plugins: [vue()],
   test: {
     environment: 'jsdom',
